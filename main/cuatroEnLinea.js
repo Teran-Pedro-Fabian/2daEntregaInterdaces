124a631ef38b37530988966f15c16e6c37939fa7
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2024-10-24 17:02:22 -0300

    inicio con las verificaciones de ganador

--- main/cuatroEnLinea.js
+++ main/cuatroEnLinea.js
@@ -1,3 +1,6 @@
+let maxcol
+let maxfil
+let condicionganar
 const j1 = 1;
 const j2 = 2;
 
@@ -44,3 +47,59 @@ function moverEnHorizontal(matriz, j1, j2) {
 
 /* console.table(cuatroEnLinea);
 console.log(cuatroEnLinea); */
+
+
+
+
+
+
+
+/* verificacion de ganador */////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* busca el ganador que se le pase */
+function buscarganador(matriz, filAgregada, colAgregada, jugador, condicionganar){
+    if(ganadorHorizontal(maxcol, filAgregada,jugador, matriz, condicionganar)){
+        return jugador
+    }
+    if(ganadorVertical(maxfil, colAgregada,jugador, matriz, condicionganar)){
+        return jugador
+    }
+
+    return 0
+}
+
+
+
+
+/* verifica si gana horizontal */
+function ganadorHorizontal(maxcol,fil, jugador, matriz, condicionganar){
+    let contador=0;
+    let estaSeguido= false;/* verifica que las fichas esten una al lado de la otra */
+    for(let i=0; i<= maxcol; i++ ){
+        if(matriz[i][col] == jugador){
+            estaSeguido = true
+        }
+        if(estaSeguido){
+            contador++
+        }else{
+            estaSeguido= false
+            contador=0
+        }
+    }
+    if(contador == condicionganar){
+        return 1
+    }else{
+        return 0
+    }
+}
+
+
+
+
+
+
+/* verifica si gana vertical */
+function ganadorVertical(maxfil, colAgregada,jugador, matriz, condicionganar){
+
+}
