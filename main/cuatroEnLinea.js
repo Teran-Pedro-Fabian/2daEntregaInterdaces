c000cdc52abbd14401542c3116dacfa62250cde6
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2024-10-29 11:28:49 -0300

    finalizado busca ganador horizontal

--- main/cuatroEnLinea.js
+++ main/cuatroEnLinea.js
@@ -1,32 +1,38 @@
 let maxcol
 let maxfil
-let condicionganar
+let condicionganar =3
 const j1 = 1;
 const j2 = 2;
 
-function crearMatriz(valorC, valorF){
+function crearMatriz(valorC, valorF) {
     let columna = valorC;
     let fila = valorF;
-    let valorNulo = 0;
-    let cuatroEnLinea = new Array(fila)
-    for (let index = 0; index < fila; index++) {
-        cuatroEnLinea[index] = new Array(columna).fill(valorNulo);
-        
-        
-    }
-    console.table(cuatroEnLinea);
-    return cuatroEnLinea;
+    maxcol=valorC;
+    maxfil=valorF;
+    /* let valorNulo =0; */
 
+    let cuatroEnLinea = new Array(fila);
+    for (let i = 0; i < fila; i++) {
+        cuatroEnLinea[i] = new Array(columna);
+        for (let j = 0; j < columna; j++) {
+            // Genera un número aleatorio entre 0 y 2 y lo asigna a la celda actual
+            cuatroEnLinea[i][j] = Math.floor(Math.random() * 3);
+        }
+    }
+
+    return cuatroEnLinea;
 }
 let matriz = crearMatriz(4, 4)
 
-console.log(crearMatriz(4,4));
-for (let i = 0; i < matriz.length; i++) {
-    for (let j = 0; j < matriz[i].length; j++) {
-      console.log(matriz[i][j]);
-    }
-  }
+console.table(matriz);
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+/* SECCION DE PRUEVAS DE FUNCIONES */
+ganadorHorizontal(1, matriz ,condicionganar)
 
+
+
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 function recorrerFila() {
     for (let i = 0; i < matriz.length; i++) {
         moverEnHorizontal(matriz[i],j1,j2);
@@ -65,6 +71,12 @@ function buscarganador(matriz, filAgregada, colAgregada, jugador, condicionganar
     if(ganadorVertical(maxfil, colAgregada,jugador, matriz, condicionganar)){
         return jugador
     }
+    if(ganadorDiagonalCreciente(maxfil, colAgregada,filAgregada,jugador, matriz, condicionganar)){
+        return jugador
+    }
+    if(ganadorDiagonalDecreciente(maxfil, colAgregada,filAgregada,jugador, matriz, condicionganar)){
+        return jugador
+    }
 
     return 0
 }
@@ -72,26 +84,38 @@ function buscarganador(matriz, filAgregada, colAgregada, jugador, condicionganar
 
 
 
-/* verifica si gana horizontal */
-function ganadorHorizontal(maxcol,fil, jugador, matriz, condicionganar){
-    let contador=0;
-    let estaSeguido= false;/* verifica que las fichas esten una al lado de la otra */
-    for(let i=0; i<= maxcol; i++ ){
-        if(matriz[i][col] == jugador){
-            estaSeguido = true
-        }
-        if(estaSeguido){
-            contador++
-        }else{
-            estaSeguido= false
-            contador=0
+/* verifica si gana horizontal, chequea todas las filas */
+function ganadorHorizontal(jugador, matriz, condicionGanar) {
+    let contador = 0;
+    let estaSeguido = false;
+
+    // Calcula los límites de filas y columnas de la matriz
+    let maxfil = matriz.length;
+    let maxcol = matriz[0].length;
+
+    for (let fil = 0; fil < maxfil; fil++) {
+        contador = 0; // Reinicia el contador para cada fila
+        estaSeguido = false;
+
+        for (let col = 0; col < maxcol; col++) {
+            if (matriz[fil][col] === jugador) {
+                estaSeguido = true;
+                contador++;
+                
+                // Si se cumple la condición de ganar, retorna 1
+                if (contador === condicionGanar) {
+                    console.log("¡Ganó!");
+                    return 1;
+                }
+            } else {
+                // Si el jugador no está en esta posición, reinicia el contador
+                estaSeguido = false;
+                contador = 0;
+            }
         }
     }
-    if(contador == condicionganar){
-        return 1
-    }else{
-        return 0
-    }
+
+    return 0; // Retorna 0 si no hay ganador en ninguna fila
 }
 
 
