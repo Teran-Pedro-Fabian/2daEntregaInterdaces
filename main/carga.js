1a2c78f4cce3c1a620057b9e0bc8e8a1402d5e11
Gonzalo Salomon <salomongonzalo8@gmail.com>
2024-10-31 15:52:06 -0300

    todo: estado 3 de la ejecucion, lo proximo es ver como pasar las variables (y en que scope pertenecen) más debuggear ejecución. tambien hay que terminar de acomodar a)ordenar el posicionamiento dentro del div clase juego, está descentrado y nos van a bajar puntos y b)no carga la imagen, hoy cargaba, ver las diferencias en el css

--- main/carga.js
+++ main/carga.js
@@ -1,3 +1,5 @@
+//Es una pantalla de carga trucha, una posta escucharía a DOMContentLoaded o usaría lazy loading para las imágenes. Por qué no la armamos? Porque esta es suficiente para la consigna
+//En un escenario real, las cosas se harían bien. No te preocupes que sabemos cómo (o lo googlearemos cuando nos toque implementarlo en un trabajo)
 let valores = [0, 10, 20, 25, 30, 40, 55, 60, 77, 80, 90, 100];
 let i = 0;
 let intervalo = setInterval(() => {
--- main/cuatroEnLinea.js
+++ main/cuatroEnLinea.js
@@ -1,3 +1,112 @@
+/*EJECUCION PRINCIPAL
+*El juego tiene 3 estados: 
+*1-Esperando para entrar a jugar
+*2-Elegir fichas y tamaño del tablero
+*3-Jugando
+*Estados 1 y 2 todavía no dibujan canvas (para hacerla menos complicada), pero sí interfaz
+*Estado 3 tiene un botoncito de reset (que reinicia el juego: reemplaza la instancia de Juego() por una en blanco) y uno de back (que resta uno al estado)
+*Transiciones:
+*1=>2 al tocar el botón de play
+*2=>3 al tocar uno de los botoncitos de modo de juego (4, 5, 6 o 7 en línea). Si no elegiste ficha lo lamento
+*3=>2 al apretar back
+*/
+
+//seteo constantes
+const j1 = 1;
+const j2 = 2;
+//agarro las secciones desde el DOM (algunas ocultas otras visibles)
+const inicio = document.querySelector("#btnJugar"); //con el botón está bien, así se renderiza el HTML base de la página en definitiva, las otras están escondidas
+const seccion2 = document.querySelector('.seccion2')
+const seccion3 = document.querySelector('.seccion3')
+
+//precargar el canvas
+const miCanvas = document.getElementById("miCanvas");
+console.log(`El canvas es ${miCanvas}`); //debug
+
+//Calculo que esto es programación orientada a eventos por los eventListener sin una clase que envuelva todo. O funcional por los callbacks, qué sé yo. Arranquemos
+const estado1 = ()=>{
+  //ESTADO 1: ver si quieren jugar => lanzar setup
+  //acá se corre todo el JS que se necesite para el juego, pero que afecte al container del juego. Lo demás, afuera se tratará
+  console.log('ESTOY EN EL ESTADO 1') //debug
+  //paso a estado 2:
+  inicio.addEventListener("click", () => {
+    inicio.classList.remove("bloque");
+    inicio.classList.add("oculto");
+    seccion2.classList.remove("oculto")
+    seccion2.classList.add('bloque')
+    console.log('PASO AL ESTADO 2') //debug
+    estado2();
+  });
+}
+
+const estado2 = ()=>{
+  /*ESTADO 2: callback para setup => lanzo juego*/
+  console.log('ESTOY EN EL ESTADO 2') //debug
+  let imagenesElegidas = {
+    jug1:'',
+    jug2:''
+  }
+  /* Funcion para seleccionar ficha */
+  let fichaCapitanAmerica = document.querySelectorAll("#idcap img");
+  console.log(fichaCapitanAmerica.length);
+
+  for (let index = 0; index < fichaCapitanAmerica.length; index++) {
+    fichaCapitanAmerica[index].addEventListener("click", () => {
+      fichaCapitanAmerica.forEach((img) => img.classList.remove("circularCapitanAmerica"));
+      fichaCapitanAmerica[index].classList.add("circularCapitanAmerica");
+      console.log(fichaCapitanAmerica[index].src)
+      console.log(typeof fichaCapitanAmerica[index].src)
+      imagenesElegidas.jug1 = fichaCapitanAmerica[index]
+    });
+  }
+  let fichaIronMan = document.querySelectorAll("#idIron img");
+  console.log(fichaCapitanAmerica.length);
+
+  for (let index = 0; index < fichaIronMan.length; index++) {
+    fichaIronMan[index].addEventListener("click", () => {
+      fichaIronMan.forEach((img) => img.classList.remove("circularIronMan"));
+      fichaIronMan[index].classList.add("circularIronMan");
+      imagenesElegidas.jug2 = fichaIronMan[index] 
+    });
+  }
+  /* se detencta la imagen elejida por cada uno de los jugadores con la clase que se le aplica respectivamente
+  circularCapitanAmerica    y   circularIronMan*/
+
+  /* traigo el valor de forma de ganar para poder iniciar el juego*/
+  const formaGanar = 0;
+  document.querySelector("btnEmpezarJuego").addEventListener("click", ()=>{
+    formaGanar= document.querySelector("formaGanar").value;
+    //escondo la seccion 2, muestro la 3
+    seccion2.classList.remove('bloque')
+    seccion2.classList.add("oculto")
+    seccion3.classList.remove("oculto")
+    seccion3.classList.add('bloque')
+    console.log('PASO AL ESTADO 3') //debug
+  })
+}
+
+const estado3 = ()=>{
+  /*ESTADO 3: callback para mantener el game loop/ ir al estado 2 (con respectivos event listener)*/
+  console.log('ESTOY EN EL ESTADO 3') //debug
+  // Obtener el contexto del canvas
+  const ctx = miCanvas.getContext("2d");
+  //instancio clase Juego y lo inicio
+  const juego = new Juego(
+    miCanvas,
+    (2*formaGanar-1),
+    (2*formaGanar-1),
+    imagenesElegidas.jug1,
+    imagenesElegidas.jug2,
+    formaGanar)
+  juego.jugar()
+}
+
+
+//Arranco en estado (del juego) 1: esperando que quieran arrancar a jugar
+estado1()
+
+/*FIN EJECUCION, ARRANCA DECLARACION DE CLASES*/
+
 class Ficha {
   constructor(x, y, radio, imagenSrc) {
     this.x = x; // Posición en el eje X
@@ -45,8 +154,6 @@ class Ficha {
 
       // Restaurar el contexto a su estado original
       ctx.restore();
-
-      
     };
   }
 }
@@ -85,141 +192,85 @@ class Juego{
         jug1: img1,
         jug2: img2
     }
+    this.crearGrid()
   }
-
-  actualizarInfoJugador(){
-    const jug1 = document.querySelector(".jug1")
-    const jug2 = document.querySelector(".jug2")
-    if(this.turno === 1){
-      jug1.classList.add("activo")
-      jug2.classList.remove("activo")
-    } else {
-      jug2.classList.add("activo")
-      jug1.classList.remove("activo")
-    }
-  }
-
-  //SETUP (estado 2, estado 3)
-  //configuro que clickeen en distintas zonas del canvas
-  configEntradas(){
-    let resetButton = document.querySelector("#reset")
-    resetButton.addEventListener()
-  }
-
-  //crea un arreglo 2d en el que vamos a ir guardando las fichas. 0 es vacio, 1 es jug1, 2 es jug2
-  crearGrid(){
-    const $tablero = document.querySelector(this.selector)
-    $tablero.innerHTML = ""
-    this.grid = []
-
-    for(let col = 0; col < this.columnas; col++){
-      for(let fila = 0; fila < this.filas; fila++){
-        this.grid[columna, fila] = 0
+  
+  //METODOS
+    //INTERNOS
+    //para cambiar de turno
+    actualizarInfoJugador(){
+      const jug1 = document.querySelector(".jug1")
+      const jug2 = document.querySelector(".jug2")
+      if(this.turno === 1){
+        jug1.classList.add("activo")
+        jug2.classList.remove("activo")
+      } else {
+        jug2.classList.add("activo")
+        jug1.classList.remove("activo")
       }
     }
-  }
+    //SETUP
+    //configuro que clickeen en distintas zonas del canvas
+    configEntradas(){
+      let resetButton = document.querySelector("#reset")
+      resetButton.addEventListener()
+    }
+    //GENERO EL TABLERO
+    //crea un arreglo 2d en el que vamos a ir guardando las fichas. 0 es vacio, 1 es jug1, 2 es jug2
+    crearGrid(){
+      const $tablero = document.querySelector(this.selector)
+      const $entrada = document.querySelector('#entrada')
+      $tablero.innerHTML = ""
+      this.grid = []
 
-  //DIBUJO (estado 2, estado 3)
-  //dibujo ficha en el x,y especificado
-  dibujarFicha(x,y,imagen){
-    ctx.drawImage(imagen, x-(img.width/2), y-(img.height/2))
-  }
+      for(let col = 0; col < this.columnas; col++){
+        for(let fila = 0; fila < this.filas; fila++){
+          this.grid[columna, fila] = 0
+        }
+      }
+    }
+    //DIBUJO (presente en estado 2, estado 3)
+    //dibujo ficha en el x,y especificado
+    dibujarFicha(x,y,imagen){
+      ctx.drawImage(imagen, x-(img.width/2), y-(img.height/2))
+    }
+    //CHEQUEO CONDICIÓN DE VICTORIA (presente en estado 3)
+    //reviso si ya gané, pero desde la posición en la que estoy (ver toda la matriz es más costoso en lectura). Si en ninguna de las direcciones se ganó, se sigue
+    chequearVictoria(col, fila){
+      return(
+        this.chequearEnDireccion(col, fila, -1, -1) + this.chequearEnDireccion(col, fila, 1, 1) - 1 == this.cantGanar || //diag desc: arriba izq+abajo der
+        this.chequearEnDireccion(col, fila, -1, 0) + this.chequearEnDireccion(col, fila, 1, 0) - 1 == this.cantGanar || //horizontal: izq+der
+        this.chequearEnDireccion(col, fila, -1, 1) + this.chequearEnDireccion(col, fila, 1, -1) - 1 == this.cantGanar || //diag crec: der arr+abajo izq
+        this.chequearEnDireccion(col, fila, 0, 1) == this.cantGanar //abajo
+      )
+    }
 
-  //CHEQUEO CONDICIÓN DE VICTORIA (estado 3)
-  //reviso si ya gané, pero desde la posición en la que estoy (ver toda la matriz es más costoso en lectura). Si en ninguna de las direcciones se ganó, se sigue
-  chequearVictoria(col, fila){
-    return(
-      1 - this.chequearEnDireccion(col, fila, -1, -1) + this.chequearEnDireccion(col, fila, 1, 1) == this.cantGanar || //diag desc: arriba izq+abajo der
-      1 - this.chequearEnDireccion(col, fila, -1, 0) + this.chequearEnDireccion(col, fila, 1, 0) == this.cantGanar || //horizontal: izq+der
-      1 - this.chequearEnDireccion(col, fila, -1, 1) + this.chequearEnDireccion(col, fila, 1, -1) == this.cantGanar || //diag crec: der arr+abajo izq
-      this.chequearEnDireccion(col, fila, 0, 1) == this.cantGanar //abajo
-    )
-  }
-
-  //revisa cuántas de la misma hay seguidas exclusivamente en la dirección indicada por varH y varV
-  chequearEnDireccion(col, fila, varH, varV, contador){
-    if (this.grid[col][fila] == this.turno){
-      contador++
-      //si ya gané, corto la ejecución
-      if (contador == this.cantGanar) 
+    //revisa cuántas de la misma hay seguidas exclusivamente en la dirección indicada por varH y varV
+    chequearEnDireccion(col, fila, varH, varV, contador){
+      if (this.grid[col][fila] == this.turno){
+        contador++
+        //si ya gané, corto la ejecución
+        if (contador == this.cantGanar) 
+          return contador
+      }
+      //si la posición que voy a revisar es válida, reviso; de otra manera ya llegué a un límite y no puedo seguir
+      if(0 <= col+varH && col+varH < this.grid.length && 0 <= fila+varV && fila+varV <this.grid[0].length)
+        return this.chequearEnDireccion(col+varH, fila+varV, varH, varV, contador)
+      else
         return contador
     }
-    //si la posición que voy a revisar es válida, reviso; de otra manera ya llegué a un límite y no puedo seguir
-    if(0 <= col+varH && col+varH < this.grid.length && 0 <= fila+varV && fila+varV <this.grid[0].length)
-      return this.chequearEnDireccion(col+varH, fila+varV, varH, varV, contador)
-    else
-      return contador
-  } 
+    //METODO PRINCIPAL
+    jugar(){
+      do{
+        //espero a que clickee en su ficha elegida
+        //pineo la ficha al mouse hasta que clickee una columna válida
+        //dejo caer la ficha
+        //chequeo victoria
+        //cambio de turno
+      this.turno = this.turno==j1?j2:j1
+      }
+      //hago un do while pq hay que ejecutar el bucle antes de checkear la condición de victoria
+      while(!this.chequearVictoria(colActual,filaActual && this.jugadaPosible))
+    }
 }
 
-/*ESTADO 2: callback para entrar y saltar a estado 3/ volver a estado 1 (para que el botón de back tenga más función que sólo volver a estado 2)*/
-//TODO 1 acá definiríamos un callback para esperar clic en botón y pasarle la info al constructor de Juego()+llamar al callback del estado 3
-/*ESTADO 3: callback para mantener el game loop/ ir al estado 2 (con respectivos event listener)*/
-//TODO 2 acá definiríamos el callback del game loop/volver 
-
-/*EJECUCION PRINCIPAL
-*El juego tiene 3 estados: 
-*1-Esperando para entrar a jugar
-*2-Elegir fichas y tamaño del tablero
-*3-Jugando
-*Estados 1 y 2 todavía no dibujan canvas (para hacerla menos complicada), pero sí interfaz
-*Estado 3 tiene un botoncito de reset (que reinicia el juego: reemplaza la instancia de Juego() por una en blanco) y uno de back (que resta uno al estado)
-*/
-
-let maxcol;
-let maxfil;
-const j1 = 1;
-const j2 = 2;
-
-//escuchar al botoncito para cargar el canvas
-const miCanvas = document.getElementById("miCanvas");
-console.log(`El canvas es ${miCanvas}`); //debug
-//ESTADO 1: ver si quieren jugar => lanzar juego
-let inicio = document.querySelector("#btnJugar");
-//acá se lanza el juego:
-inicio.addEventListener("click", () => {
-  inicio.classList.remove("bloque");
-  document.querySelector(".seccion2").classList.remove("oculto")
-  inicio.classList.add("oculto");
-  miCanvas.classList.remove("oculto");
-  miCanvas.classList.add("bloque");
-  config();
-});
-
-// Obtener el contexto del canvas
-const ctx = miCanvas.getContext("2d");
-
-//estado 3 se dispararía con un new Juego() según lo que configuren
-//y con un jugar, ver nombres
-
-
-/* Funcion para seleccionar ficha *////////////////////////////////////////////
-
-let fichaCapitanAmerica = document.querySelectorAll("#idcap img");
-console.log(fichaCapitanAmerica.length);
-
-for (let index = 0; index < fichaCapitanAmerica.length; index++) {
-  fichaCapitanAmerica[index].addEventListener("click", () => {
-    fichaCapitanAmerica.forEach((img) => img.classList.remove("circularCapitanAmerica"));
-    fichaCapitanAmerica[index].classList.add("circularCapitanAmerica");
-    imagenesElegidas.jug1 = fichaCapitanAmerica[index]
-  });
-}
-let fichaIronMan = document.querySelectorAll("#idIron img");
-console.log(fichaCapitanAmerica.length);
-
-for (let index = 0; index < fichaIronMan.length; index++) {
-  fichaIronMan[index].addEventListener("click", () => {
-    fichaIronMan.forEach((img) => img.classList.remove("circularIronMan"));
-    fichaIronMan[index].classList.add("circularIronMan");
-    imagenesElegidas.jug2 = fichaIronMan[index] 
-  });
-}
-/* se detencta la imagen elejida por cada uno de los jugadores con la clase que se le aplica respectivamente
- circularCapitanAmerica    y   circularIronMan*/
-
-/* traigo el valor de forma de ganar para poder  */
-let formaGanar = 0;
-document.querySelector("btnEmpezarJuego").addEventListener("click", ()=>{
-  formaGanar= document.querySelector("formaGanar").value;
-})
